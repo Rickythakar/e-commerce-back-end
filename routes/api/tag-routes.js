3212c0a0c9da890012fa5f46cb09b4f6b919f61f
Ricky Thakar <rickythakar@gmail.com>
2022-05-30 23:26:22 -0400

    updated tag-routes and tested models

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,14 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    // be sure to include its associated Product data
+    include:{
+      model: Product,
+      attributes: ['product_name','price','stock','category_id' ]
+    }
+  })
+  
 });
 
 router.get('/:id', (req, res) => {
